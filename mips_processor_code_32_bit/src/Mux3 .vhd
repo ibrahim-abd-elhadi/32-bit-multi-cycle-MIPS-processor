9f17fab685b80756c4b254032438d8c1707e25f9
Ibrahim <147931882+eprahimayman@users.noreply.github.com>
2025-05-10 18:42:08 +0300

    Update Mux3 .vhd

--- mips_processor_code_32_bit/src/Mux3 .vhd
+++ mips_processor_code_32_bit/src/Mux3 .vhd
@@ -8,19 +8,14 @@ entity Mux3 is
         input_2    : in std_logic_vector(31 downto 0);
         input_3    : in std_logic_vector(31 downto 0);
         mux_select : in std_logic_vector(1 downto 0);
-
         -- output
         output     : out std_logic_vector(31 downto 0) );
 end Mux3;
-
 architecture Behavioral of Mux3 is
-
-  signal mux_out : std_logic_vector(31 downto 0);
 begin
   with mux_select select
-    mux_out <= input_1 when "00",
+     output	<= input_1 when "00",
                input_2 when "01",
                input_3 when "10",
-               "00000000000000000000000000000000" when others;
-  output <= mux_out;
+               (others => '0') when others;
 end Behavioral;
